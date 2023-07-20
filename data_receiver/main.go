b1487901043cac5b5b33824ca3dd5cfc9aa2af75
Vitaliy Boyanivskyy <vitalik.boyan@gmail.com>
2023-07-20 20:56:03 +0300

    setup

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -5,20 +5,51 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/boyanivskyy/toll-calculator/types"
 	"github.com/gorilla/websocket"
 )
 
 func main() {
-	fmt.Println("data receiver working fine")
+	receiver := NewDataReceiver()
+	http.HandleFunc("/ws", receiver.handleWS)
+	http.ListenAndServe(":30000", nil)
 }
 
 type DataReceiver struct {
-	msg chan string
+	msgch chan types.OBUData
+	conn  *websocket.Conn
 }
 
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+func NewDataReceiver() *DataReceiver {
+	return &DataReceiver{
+		msgch: make(chan types.OBUData, 128),
+	}
+}
+
+func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1028,
+		WriteBufferSize: 1028,
+	}
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Fatal(err)
+	}
+	dr.conn = conn
+
+	go dr.wsReceiveLoop()
+}
+
+func (dr *DataReceiver) wsReceiveLoop() {
+	fmt.Println("New OBU client(data_receiver) connected!")
+	for {
+		data := types.OBUData{}
+		if err := dr.conn.ReadJSON(&data); err != nil {
+			log.Println("read ws data error:", err)
+			continue
+		}
+		fmt.Printf("received OBU data from [%d]:: <lat %.2f, long %.2f> \n", data.OBUID, data.Lat, data.Long)
+
+		dr.msgch <- data
 	}
 }
