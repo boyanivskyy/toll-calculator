999172d60a336fce94b05e2945228952900aef3e
Vitaliy Boyanivskyy <vitalik.boyan@gmail.com>
2023-07-23 09:27:12 +0300

    refactor and finish implementing kafka producer

---
+++ data_receiver/middleware.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"time"
+
+	"github.com/boyanivskyy/toll-calculator/types"
+	"github.com/sirupsen/logrus"
+)
+
+type LogMiddleware struct {
+	next DataProducer
+}
+
+func NewLoggingMiddleware(next DataProducer) *LogMiddleware {
+	return &LogMiddleware{
+		next: next,
+	}
+}
+
+func (l *LogMiddleware) ProduceData(data types.OBUData) error {
+	defer func(start time.Time) {
+		logrus.WithFields(logrus.Fields{
+			"obuId": data.OBUID,
+			"lat":   data.Lat,
+			"long":  data.Long,
+			"took":  time.Since(start),
+		}).Info("producing to kafka")
+	}(time.Now())
+
+	return l.next.ProduceData(data)
+}
